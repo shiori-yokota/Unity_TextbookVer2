7b855185fb2024a03b4256d01670af6271643726
shiori yokota <e1252225@gmail.com>
2018-02-27 20:30:43 +0900

    Modify : tag manager and python sources

---
+++ Python/Sources/PatternRecognition.py
@@ -0,0 +1,6 @@
+﻿import sys
+sys.path.append(PYTHON_LIB_PATH)
+import random
+
+################ main ###############
+
--- Python/Sources/test.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append(PYTHON_LIB_PATH)
-import random
-import os.path
-if os.path.exists('Python/Sources/qvalues.txt'):
-	filename = 'Python/Sources/qvalues.txt'
-else:
-	filename = ''
